3d2c84c9691e8607705d2a6a325152ce579b488e
Alessio-ferrara <66694308+Alessio-ferrara@users.noreply.github.com>
2022-11-04 22:14:03 +0100

    Added cors handling

--- stressWork/stressWork/settings.py
+++ stressWork/stressWork/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "employee",
     "employer"
 ]
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
 ]
 
 ROOT_URLCONF = "stressWork.urls"
