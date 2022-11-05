bee97b87c078fc72f8efcc36553839e4c5bbc06e
Alessio-ferrara <66694308+Alessio-ferrara@users.noreply.github.com>
2022-11-05 09:54:56 +0100

    Changed requirements and setup api for retrieving the recording

--- stressWork/employee/urls.py
+++ stressWork/employee/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import EmployeeAPIView, EmployeeDetailAPIView
+from .views import EmployeeAPIView, EmployeeDetailAPIView, NewRecordAPIView
 
 employee_api = EmployeeAPIView.as_view()
 employee_detail_api = EmployeeDetailAPIView.as_view()
+new_rercoding = NewRecordAPIView.as_view()
 
 urlpatterns = [
     path("", employee_api, name="employees"),
     path("<int:employee_id>", employee_detail_api, name="employee-detail"),
+    path("newRecording", new_rercoding, name="new-recording" )
 ]
